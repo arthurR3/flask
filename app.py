027e23f161307e96acf9b194a6415d5e5dc2d6ad
arthurR3 <hedzjose014@gmail.com>
2024-06-25 07:55:49 -0600

    update my project

--- app.py
+++ app.py
@@ -1,43 +1,42 @@
 from flask import Flask, request, render_template, jsonify
+import numpy as np
 import joblib
-import pandas as pd
-import logging
 
 app = Flask(__name__)
 
-# Configurar el registro
-logging.basicConfig(level=logging.DEBUG)
-
-# Cargar el modelo entrenado
+# Cargar el modelo y el scaler
 model = joblib.load('modelo.pkl')
-app.logger.debug('Modelo cargado correctamente.')
 
 @app.route('/')
-def home():
+def index():
     return render_template('formulario.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Obtener los datos enviados en el request
-        majorLength = float(request.form['MajorAxisLength'])
-        minorLength = float(request.form['MinorAxisLength'])
-        perimetro = float(request.form['Perimeter'])
-        
-        # Crear un DataFrame con los datos
-        data_df = pd.DataFrame([[majorLength, minorLength, perimetro]], columns=['MajorAxisLength', 'MinorAxisLength', 'Perimeter'])
-        app.logger.debug(f'DataFrame creado: {data_df}')
-        
-        # Realizar predicciones
-        prediction = model.predict(data_df)
-        app.logger.debug(f'Predicción: {prediction[0]}')
-        
-        # Devolver las predicciones como respuesta JSON
-        return jsonify({'categoria': prediction[0]})
+        # Obtener datos del formulario
+        data = request.form.to_dict(flat=True)
+        age = float(data['age'])
+        cement = float(data['cement'])
+        superplasticizer = float(data['superplasticizer'])
+        fineaggregate = float(data['fineaggregate'])
+        flyash = float(data['flyash'])
+
+        # Crear un array numpy con los datos
+        input_data = np.array([[age, cement, superplasticizer, fineaggregate, flyash]])
+
+        # Estandarizar los datos
+
+        # Realizar la predicción
+        prediction = model.predict(input_data)
+
+        # Convertir la predicción a una respuesta legible
+        result = prediction[0]
+
+        return jsonify({'prediction': result})
+
     except Exception as e:
-        app.logger.error(f'Error en la predicción: {str(e)}')
-        return jsonify({'error': str(e)}), 400
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
